a5124c9e08ed4834b502e41c50871b8fca11b69c
caffeinebloom <dcoulson@caffeinebloom.com>
2021-01-18 22:25:57 -0600

    first commit of to do application by David Coulson

---
+++ ToDo/src/app/main/main.component.ts
@@ -0,0 +1,38 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  toDoItem: any;
+  toDoList: any;
+  entry: string;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.toDoList = new Array();
+    this.entry = '';
+  }
+
+  addToList(text){
+    console.log(text)
+    console.log(this.entry)
+    if(text !== ''){
+      const task = {
+        title: text,
+        completed: false
+      }
+      this.toDoList.push(task)
+      console.log(this.toDoList)
+      this.entry = null;
+    }
+    else{
+      alert('Enter Task!')
+    }
+
+  }
+
+}
